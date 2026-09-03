2e639c94163c6c46c0b73ec31a006b0e00172952
Akash Yadav <akash16yadav2004@gmail.com>
2026-09-04 01:32:05 +0530

    Update AiDoc\src\main\java\com\aiDoc\configure\Config.java

--- AiDoc\src\main\java\com\aiDoc\configure\Config.java
+++ AiDoc\src\main\java\com\aiDoc\configure\Config.java
@@ -1,1 +1,16 @@
 
+
+package com.aiDoc.AiDoc.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
